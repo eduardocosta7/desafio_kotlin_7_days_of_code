e8e69ec0401965b23bbf91f26b3d815ea79714fe
Eduardo F Costa <e.feitosa@spotpromo.com.br>
2022-11-15 18:00:00 -0300

    primeiro dia

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,26 +1,33 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+    val modifierImage = Modifier.size(350.dp)
+    val contentImage = ContentScale.Fit
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        Column (modifier = Modifier.verticalScroll(rememberScrollState())) {
+            Text(text = "Á Espera de um Milagre", fontSize = 22.sp)
+            Image(painterResource("drawables/capa.jpeg"), "Capa", modifier =  modifierImage, contentScale =  contentImage, alignment = Alignment.CenterStart)
+            Text("Nota: 8.6 - Ano: 1999", fontSize = 18.sp)
         }
     }
 }
