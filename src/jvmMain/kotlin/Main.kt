36b3791de11f69471c4a60517aabb55c56406971
Eduardo F Costa <e.feitosa@spotpromo.com.br>
2022-11-16 11:03:32 -0300

    terceiro dia

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,40 +1,64 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.toComposeImageBitmap
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import org.jetbrains.skia.Image.Companion.makeFromEncoded
+import org.jetbrains.skia.Image
 import java.net.URL
 
 @Composable
 @Preview
 fun App() {
-    val image = "https://bankshop.com.br/wp-content/uploads/2021/10/2020-1344-a-espera-milagre-poster.jpg"
+    val imageCapa = "https://bankshop.com.br/wp-content/uploads/2021/10/2020-1344-a-espera-milagre-poster.jpg"
+    val imageStar = "https://cdn-icons-png.flaticon.com/512/686/686384.png"
 
-    MaterialTheme {
-        Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-            Text(text = "Á Espera de um Milagre", fontSize = 22.sp)
-            Image(
-                bitmap = image.loadImageBitmap(),
-                "Capa",
-                modifier = Modifier.size(350.dp),
-                contentScale = ContentScale.Fit,
-                alignment = Alignment.CenterStart
-            )
-            Text("Nota: 8.6 - Ano: 1999", fontSize = 18.sp)
+    MaterialTheme(colors = darkColors()) {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            Box(modifier = Modifier.wrapContentSize()) {
+                Column(modifier = Modifier.align(Alignment.Center).matchParentSize()) {
+                    Image(
+                        bitmap = imageCapa.loadImageBitmap(),
+                        null,
+                        modifier = Modifier.size(350.dp).padding(5.dp),
+                        contentScale = ContentScale.Fit
+                    )
+                    Row(modifier = Modifier.width(240.dp).align(Alignment.CenterHorizontally).padding(5.dp)) {
+                        Row(modifier = Modifier.align(Alignment.CenterVertically)) {
+                            Icon(
+                                bitmap = imageStar.loadImageBitmap(),
+                                null,
+                                modifier = Modifier.height(20.dp).padding(end = 10.dp),
+                                tint = Color.Unspecified
+                            )
+                        }
+                        Text(
+                            "8.6",
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier.padding(end = 130.dp)
+                        )
+                        Text("1999", fontSize = 18.sp)
+                    }
+                    Text(
+                        text = "Á Espera de um Milagre",
+                        fontSize = 22.sp,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.align(Alignment.CenterHorizontally).padding(5.dp).width(240.dp)
+                    )
+                }
+            }
         }
     }
 }
@@ -47,5 +71,5 @@ fun main() = application {
 
 fun String.loadImageBitmap(): ImageBitmap {
     val byteArray = URL(this).openStream().use { it.readBytes() }
-    return makeFromEncoded(byteArray).toComposeImageBitmap()
+    return Image.makeFromEncoded(byteArray).toComposeImageBitmap()
 }
